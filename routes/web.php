82695212b62cf32bd549575eba34e7d9891fa45d
MuQi97 <muq971118@gmail.com>
2020-02-12 19:47:29 +0800

    retrieve password success message prompt

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,5 @@
 
 Route::get('/', 'PagesController@root')->name('root');
 Auth::routes(['verify' => true]);
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
