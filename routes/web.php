7dd2cfa50ea75b35aee1bd2fb592dab179a9848f
MuQi97 <muq971118@gmail.com>
2020-02-13 14:52:26 +0800

    show user page model

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@
 */
 
 Route::get('/', 'PagesController@root')->name('root');
+
 Auth::routes(['verify' => true]);
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
