6b6e28a16270e758941fc319b45c034d6f111bf1
MuQi97 <muq971118@gmail.com>
2020-02-13 21:33:53 +0800

    Classified topic list

--- app/helpers.php
+++ app/helpers.php
@@ -5,6 +5,11 @@ function route_class()
     return str_replace('.', '-', Route::currentRouteName());
 }
 
+function category_nav_active($category_id)
+{
+    return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
+}
+
 function get_db_config()
 {
     if (getenv('IS_IN_HEROKU')) {
