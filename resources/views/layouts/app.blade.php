d7ab696476eb0b0f4d29aca6c5bbccc66e5a72b7
MuQi97 <muq971118@gmail.com>
2020-02-14 12:13:00 +0800

    Topic content page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', 'MuQ BBS') - MuQ BBS</title>
+    <meta name="description" content="@yield('description', 'MuQBBS 爱好者社区')" />
 
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
