1a602ab123f7f4d5c4358685b25ef16890cb612a
MuQi97 <muq971118@gmail.com>
2020-02-14 11:38:44 +0800

    User can create post

--- app/helpers.php
+++ app/helpers.php
@@ -1,15 +1,5 @@
 <?php
 
-function route_class()
-{
-    return str_replace('.', '-', Route::currentRouteName());
-}
-
-function category_nav_active($category_id)
-{
-    return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
-}
-
 function get_db_config()
 {
     if (getenv('IS_IN_HEROKU')) {
@@ -31,4 +21,20 @@ function get_db_config()
             'password'  => env('DB_PASSWORD', ''),
         ];
     }
+}
+
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
+
+function category_nav_active($category_id)
+{
+    return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
+}
+
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return Str::limit($excerpt, $length);
 }
